267b4612ee4a9195d07939bc822a17827e333a3f
Maria Golubeva <mitsunari8ishida@gmail.com>
2021-07-31 12:56:28 +0300

    hooks added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import shortid from "shortid";
+
+import ContactsList from "./Components/ContactsList/ContactsList";
+import Form from "./Components/Form";
+import Filter from "./Components/Filter";
+import ListItem from "./Components/ContactsList/ListItem";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    if (localStorage.contacts) {
+      const contactsFromLocalStorage = JSON.parse(
+        localStorage.getItem("contacts")
+      );
+      setContacts(contactsFromLocalStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const FilterHandler = (value) => {
+    setFilter(value);
+  };
+
+  const addContact = (data) => {
+    const newContact = {
+      id: shortid.generate(),
+      name: data.name,
+      number: data.number,
+    };
+    setContacts((prev) => [...prev, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    const newContacts = contacts.filter((item) => item.id !== id);
+    setContacts(newContacts);
+  };
+
+  const filtredContacts = contacts.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Form sendData={addContact} />
+      <Filter onChange={FilterHandler} value={filter} />
+      <ContactsList>
+        <ListItem contacts={filtredContacts} onClick={deleteContact} />
+      </ContactsList>
     </div>
   );
 }
